2cbe2963a5c7a13c3a883490a13a25b1e9092674
Dustin Gallo <48537601+gallodustin@users.noreply.github.com>
2023-08-25 17:00:48 -0500

    simple delete endpoint

--- main.py
+++ main.py
@@ -44,3 +44,11 @@ def create_posts(post: Post, status_code=status.HTTP_201_CREATED):
     post_dict['id'] = randrange(0, 1000000) # this is a goofy example, but sure
     my_posts.append(post_dict)
     return {'data': post_dict}
+
+@app.delete('/posts/{id}')
+def delete_post(id: int):
+    found_post = find_post(id)
+    if not found_post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post {id} was not found.')
+    my_posts.remove(found_post)
+    return {'message': f'post {id} was deleted.'}
