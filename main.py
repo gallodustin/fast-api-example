01faef3295d47947359d9c00516ce6c4f5b8fbda
Dustin Gallo <48537601+gallodustin@users.noreply.github.com>
2023-08-25 14:58:28 -0500

    simple second endpoint

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+@app.get('/')
+def root():
+    return {'message': 'Miami is nice'}
+
+@app.get('/posts')
+def get_posts():
+    return {'data': 'Here are your posts.'}
