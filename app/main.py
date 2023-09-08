aa9c767e995fa7344438a02d2f3ddb7e027709a1
Dustin Gallo <48537601+gallodustin@users.noreply.github.com>
2023-09-08 16:39:46 -0500

    more robust PostResponse schema
    * haven't tested this change yet

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from psycopg2.extras import RealDictCursor
 from sqlalchemy.orm import Session
 from . import models, schemas
 from .database import engine, get_db
+from typing import List
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
@@ -20,30 +21,23 @@ except Exception as e:
 def root():
     return {'message': 'Miami is nice'}
 
-# test to see if sqlalchemy is working
-# we need this db parameter in order to interact with the DB using SQLAlchemy
-@app.get('/sqlalchemy')
-def test_posts(db: Session = Depends(get_db)):
-    # db.query(models.Post) returns a SQLAlchemy query object and all() gives the results
-    return db.query(models.Post).all()
-
-@app.get('/posts')
+@app.get('/posts', response_model=List[schemas.PostResponse])
 def get_posts(db: Session = Depends(get_db)):
     # cursor.execute('SELECT * FROM posts;')
     # posts = cursor.fetchall()
     posts = db.query(models.Post).all()
-    return {'data': posts}
+    return posts
 
-@app.get('/posts/{id}')
+@app.get('/posts/{id}', response_model=schemas.PostResponse)
 def get_post(id: int, db: Session = Depends(get_db)):
     # cursor.execute('SELECT * FROM posts WHERE id = %s;', (str(id),))
     # post = cursor.fetchone()
     post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Post {id} was not found.')
-    return {'data': post}
+    return post
 
-@app.post('/posts', status_code=status.HTTP_201_CREATED)
+@app.post('/posts', status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
 def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     # passing as f string here would be bad because it allows sql injection
     # cursor.execute() checks for this before doing the substitution
@@ -53,7 +47,7 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     new_post = models.Post(**post.model_dump())
     db.add(new_post) # add the new post to the session
     db.commit() # commit the session to the DB
-    return {'data': new_post}
+    return new_post
 
 @app.delete('/posts/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db)):
@@ -67,7 +61,7 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-@app.put('/posts/{id}')
+@app.put('/posts/{id}', response_model=schemas.PostResponse)
 def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
     # cursor.execute('UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *;', (post.title, post.content, post.published, str(id)))
     # updated_post = cursor.fetchone()
@@ -78,4 +72,4 @@ def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)
     # conn.commit()
     updated_post_query.update(post.model_dump(), synchronize_session=False)
     db.commit()
-    return {'message': 'post updated.', 'data': updated_post}
+    return updated_post
--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 # Will be useful later as a base class for different requests using a Post
 class PostBase(BaseModel):
@@ -8,3 +9,13 @@ class PostBase(BaseModel):
 
 class PostCreate(PostBase):
     pass
+
+class PostResponse(PostBase):
+    id: int
+    title: str
+    content: str
+    published: bool
+    created_at: datetime
+
+    class Config: 
+        orm_mode = True # so that we can use SQLAlchemy models with Pydantic which normally uses dict
