858bf0231c6be0a8ebd548903da785f3bccd8056
Dustin Gallo <48537601+gallodustin@users.noreply.github.com>
2023-09-08 14:20:53 -0500

    update endpoints to use SQL

--- app/main.py
+++ app/main.py
@@ -1,66 +1,65 @@
-from typing import Optional
-from fastapi import FastAPI, HTTPException, Response, status
-from pydantic import BaseModel
 from random import randrange
 
-app = FastAPI()
+from fastapi import FastAPI, HTTPException, Response, status
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from pydantic import BaseModel
 
+app = FastAPI()
 
 class Post(BaseModel):
     title: str
     content: str
     published: bool = True
-    rating: Optional[int] = None
 
-# start with some example post for now, and use memory as the "DB"
-my_posts = []
-my_posts.append({'title': 'title of post 1', 'content': 'content of post 1', 'id': 1})
-my_posts.append({'title': 'favorite foods', 'content': 'i like pizza', 'id': 2})
-
-# don't do this lol
-def find_post(id):
-    for p in my_posts:
-        if p['id'] == id:
-            return p
-
+try:
+    conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres', password='postgres', cursor_factory=RealDictCursor)
+    cursor = conn.cursor()
+    print('Database connection was successful.')
+except Exception as e:
+    print(f'Database connection failed: {e}')
+    
 @app.get('/')
 def root():
     return {'message': 'Miami is nice'}
 
 @app.get('/posts')
 def get_posts():
-    return {'data': my_posts}
+    cursor.execute('SELECT * FROM posts;')
+    posts = cursor.fetchall()
+    return {'data': posts}
 
 @app.get('/posts/{id}')
 def get_post(id: int):
-    found_post = find_post(id)
-    if not found_post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post {id} was not found.')
-    return {'post_detail': found_post}
+    cursor.execute('SELECT * FROM posts WHERE id = %s;', (str(id),))
+    post = cursor.fetchone()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Post {id} was not found.')
+    return {'data': post}
 
 @app.post('/posts')
 def create_posts(post: Post, status_code=status.HTTP_201_CREATED):
-    post_dict = post.model_dump()
-    post_dict['id'] = randrange(0, 1000000) # this is a goofy example, but sure
-    my_posts.append(post_dict)
-    return {'data': post_dict}
+    # passing as f string here would be bad because it allows sql injection
+    # cursor.execute() checks for this before doing the substitution
+    cursor.execute('INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *;', (post.title, post.content, post.published))
+    new_post = cursor.fetchone()
+    conn.commit() # otherwise the changes won't persist in the DB
+    return {'data': new_post}
 
 @app.delete('/posts/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-    found_post = find_post(id)
-    if not found_post:
+    cursor.execute('DELETE FROM posts WHERE id = %s RETURNING *;', (str(id),))
+    deleted_post = cursor.fetchone()
+    if not deleted_post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post {id} was not found.')
-    my_posts.remove(found_post)
-    return {'message': f'post {id} was deleted.'}
+    conn.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put('/posts/{id}')
 def update_post(id: int, post: Post):
-    print(post)
-    found_post = find_post(id)
-    if not found_post:
+    cursor.execute('UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *;', (post.title, post.content, post.published, str(id)))
+    updated_post = cursor.fetchone()
+    if not updated_post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post {id} was not found.')
-    found_post['title'] = post.title
-    found_post['content'] = post.content
-    found_post['published'] = post.published
-    found_post['rating'] = post.rating
-    return {'message': 'post updated.'}
+    conn.commit()
+    return {'message': 'post updated.', 'data': updated_post}
